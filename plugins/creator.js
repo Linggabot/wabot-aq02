7ab1c901efbeb05011f57ba45462a0096470f2f0
Linggabot <94733123+Linggabot@users.noreply.github.com>
2021-11-21 09:16:57 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
-  // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
-  this.sendContact(m.chat, '6289647417373', 'Bintangp02', m)
+  // F this.sendContact(m.chat, '6281477094099', 'lingga', m)
+  this.sendContact(m.chat, '6281477094099', 'lingga pratama"', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
